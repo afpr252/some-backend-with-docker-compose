50bf2799a978f4fa3251ebee2944a30be276dc08
André Rodrigues <afpr252@gmail.com>
2024-03-02 12:00:15 +0000

    Clean up repo

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM ubuntu:22.04
+
 ARG A
 RUN echo "A is $A"
+
 RUN apt-get update && apt-get -y install netcat
+
 ENTRYPOINT ["nc", "-v", "-k", "-l"]
--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e -u -o pipefail
-
-echo 'script.sh has been executed'
-echo "$A"
-sleep 10 # simulating the script takes a while
